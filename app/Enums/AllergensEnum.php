aa591ce63c7030250ecc6901fdab7c12c2848713
Hawraa Atat <104064533+HawraaAtat@users.noreply.github.com>
2024-04-21 16:50:13 +0300

    Enum: Fixing names

--- app/Enums/AllergensEnum.php
+++ app/Enums/AllergensEnum.php
@@ -11,17 +11,16 @@ use BenSampo\Enum\Enum;
  */
 final class AllergensEnum extends Enum
 {
-    const DAIRY = 1;
-    const EGG = 2;
-    const GLUTEN = 3;
-    const GRAIN = 4;
-    const PEANUT = 5;
-    const SEAFOOD = 6;
-    const SESAME = 7;
-    const SHELLFISH = 8;
-    const SOY = 9;
-    const SULFITE = 10;
-    const TREE_NUT = 11;
-    const WHEAT = 12;
-
+    const DAIRY = 'Dairy';
+    const EGG = 'Egg';
+    const GLUTEN = 'Gluten';
+    const GRAIN = 'Grain';
+    const PEANUT = 'Peanut';
+    const SEAFOOD = 'Seafood';
+    const SESAME = 'Sesame';
+    const SHELLFISH = 'Shellfish';
+    const SOY = 'Soy';
+    const SULFITE = 'Sulfite';
+    const TREE_NUT = 'Tree Nut';
+    const WHEAT = 'Wheat';
 }
