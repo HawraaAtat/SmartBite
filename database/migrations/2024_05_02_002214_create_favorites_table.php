9be90f811886e1e755bfc0804f00e4c548a2a0b5
Hawraa Atat <104064533+HawraaAtat@users.noreply.github.com>
2024-05-07 23:48:14 +0300

    favorites table: fixing the type from foreignId -> int

--- database/migrations/2024_05_02_002214_create_favorites_table.php
+++ database/migrations/2024_05_02_002214_create_favorites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('item_id')->constrained()->onDelete('cascade');
+            $table->integer('item_id');
             $table->timestamps();
         });
     }
