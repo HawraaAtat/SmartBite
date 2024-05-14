71c8daa38acb9f8c2d4d7951f321e5930ce5291b
Hawraa Atat <104064533+HawraaAtat@users.noreply.github.com>
2024-05-14 19:06:10 +0300

    fix filter css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -202,18 +202,19 @@
                         <div class="search-box input-group input-radius input-rounded input-lg">
                             <input type="text" name="search" placeholder="Search beverages or foods" class="form-control">
                             <span class="input-group-text">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                {{-- <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9.65925 19.3102C11.8044 19.3103 13.8882 18.5946 15.5806 17.2764L21.9653 23.6612C22.4423 24.1218 23.2023 24.1086 23.663 23.6316C24.1123 23.1664 24.1123 22.4288 23.663 21.9635L17.2782 15.5788C20.5491 11.3682 19.7874 5.30333 15.5769 2.03243C11.3663 -1.23848 5.30149 -0.476799 2.03058 3.73374C-1.24033 7.94428 -0.478646 14.0092 3.73189 17.2801C5.42702 18.5969 7.51269 19.3113 9.65925 19.3102ZM4.52915 4.5273C7.36245 1.69394 11.9561 1.69389 14.7895 4.5272C17.6229 7.3605 17.6229 11.9542 14.7896 14.7876C11.9563 17.6209 7.36261 17.621 4.52925 14.7877C4.5292 14.7876 4.5292 14.7876 4.52915 14.7876C1.69584 11.9749 1.67915 7.39794 4.49181 4.56464C4.50424 4.55216 4.51667 4.53973 4.52915 4.5273Z" fill="#C9C9C9"/>
-                                </svg>
+                                </svg> --}}
+                                <!-- Filter Button -->
+                                <button class="btn btn-filter" type="button" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="white">
+                                        <path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </button>
                             </span>
                         </div>
-                        <!-- Filter Button -->
-                        <button class="btn btn-filter" type="button" data-bs-toggle="offcanvas"
-                                    data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="white">
-                                <path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        </button>
+                        <button type="submit" class="btn btn-sm btn-primary" style="height: 50px; margin-bottom: 20px; border-radius: 50px;">Search</button>
                     </div>
                     <!-- Filters -->
                     <div class="row" id="contentArea">
@@ -638,11 +639,6 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-sm btn-danger light"
-                                            style="margin-right: 3px;" data-bs-dismiss="offcanvas">Close</button>
-                                        <button type="submit" class="btn btn-sm btn-primary">Search</button>
-                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -673,15 +669,13 @@
 
                     .btn-filter {
                         padding: 5px 10px; /* Smaller padding */
-                        background-color: #04764E; /* Customize button color */
-                        color: #fff;
+                        color: #000;
                         border: none;
                         border-radius: 50%; /* Rounded button */
                         cursor: pointer;
                         display: flex;
                         align-items: center;
                         justify-content: center;
-                        margin-bottom: 20px;
                         height: 50px;
                         width: 50px;
                     }
@@ -689,10 +683,6 @@
                     .btn-filter svg {
                         width: 16px; /* Smaller icon size */
                         height: 16px;
-                    }
-
-                    .btn-filter:hover {
-                        background-color: #036b42; /* Adjust hover color as needed */
                     }
 
                     .offcanvas-top {
